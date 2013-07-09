b257bd438bc7d0d0fea27eedcc4cc7157a1abc23
chentong <chentongtc@gmail.com>
2013-07-09 11:48:37 +0100

    added runner for intellij, updated gitignore

--- scalatestgradle/scalatest/PersonSpec.scala
+++ scalatestgradle/scalatest/PersonSpec.scala
@@ -2,7 +2,10 @@ import collection.mutable.Stack
 import org.scalatest._
 import org.scalatest.matchers._
 import org.gradle.Person
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class PersonSpec extends FlatSpec with ShouldMatchers {
   
   "A Person" should "have a name" in {
